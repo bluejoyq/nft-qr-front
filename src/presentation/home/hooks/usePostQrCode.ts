5108b582c5f7e768e908ad453a484e84d7b7baa4
bluejoyq <bluejoy32@gmail.com>
2023-10-24 17:12:48 +0900

    refactor: post hook 분리

---
+++ src/presentation/home/hooks/usePostQrCode.ts
@@ -0,0 +1,22 @@
+import { postQRCode } from "@/data/backend";
+import { useMutation } from "@tanstack/react-query";
+import { NftQrForm } from "./useNftQrFormContext";
+
+export const usePostQrCode = () => {
+  return useMutation({
+    mutationFn: async (data: NftQrForm) => {
+      const imageUrl = data.nft.rawMetadata?.image?.replace(
+        "ipfs://",
+        "https://ipfs.io/ipfs/"
+      );
+      if (imageUrl == null) {
+        throw new Error("NFT does not have metadata");
+      }
+      const resultImage = await postQRCode({
+        imageUrl,
+        qrData: data.qrData,
+      });
+      return resultImage;
+    },
+  });
+};
--- src/presentation/home/steps/index.tsx
+++ src/presentation/home/steps/index.tsx
@@ -2,51 +2,31 @@ import { ReactElement, useEffect, useState } from "react";
 import { SelectNftStep } from "./SelectNftStep";
 import { GeneratingStep } from "./GeneratingStep";
 import { FormProvider, useForm } from "react-hook-form";
-import { useMutation } from "@tanstack/react-query";
-import { postQRCode } from "@/data/backend";
 import { ResultStep } from "./ResultStep";
 import { NftQrForm } from "../hooks/useNftQrFormContext";
 import { DataStep } from "./DataStep";
+import { usePostQrCode } from "../hooks/usePostQrCode";
 
 type Step = "selectNft" | "qrData" | "generating" | "done";
 interface StepsProps {
   address: string;
 }
 
-const usePostQrCode = () => {
-  return useMutation({
-    mutationFn: async (data: NftQrForm) => {
-      const imageUrl = data.nft.rawMetadata?.image?.replace(
-        "ipfs://",
-        "https://ipfs.io/ipfs/"
-      );
-      if (imageUrl == null) {
-        throw new Error("NFT does not have metadata");
-      }
-      const resultImage = await postQRCode({
-        imageUrl,
-        qrData: data.qrData,
-      });
-      return resultImage;
-    },
-  });
-};
-
 export const Steps = ({ address }: StepsProps): ReactElement => {
   const initialStep = "selectNft";
   const [step, setStep] = useState<Step>(initialStep);
   const methods = useForm<NftQrForm>();
   const { handleSubmit, watch } = methods;
-  const { mutate, isLoading, data: resultImage } = usePostQrCode();
+  const { mutate, isPending, data: resultImage } = usePostQrCode();
 
   useEffect(() => {
     if (resultImage) {
       setStep("done");
     }
-    if (isLoading) {
+    if (isPending) {
       setStep("generating");
     }
-  }, [isLoading, resultImage]);
+  }, [isPending, resultImage]);
   const onSubmit = (data: NftQrForm) => {
     mutate(data);
   };
