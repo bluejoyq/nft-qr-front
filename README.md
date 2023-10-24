c7ba6d75136b4b7f23823399123f7cf3e0980d8a
bluejoyq <bluejoy32@gmail.com>
2023-10-24 18:48:00 +0900

    feat: add firebase

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## 목적
 ![](/docs/before.jpeg)
 ![](docs/after.png)
+
 이 어플리케이션은 사용자의 EVM NFT로부터, 메타 데이터 이미지와 잘 어울리는 QR 코드를 생성함을 목표로 합니다.
 `Generative AI` 중 하나인 `Stable Diffusion v2.1`과 [controlnet_qrcode](https://huggingface.co/DionTimmer/controlnet_qrcode-control_v11p_sd21)를 사용해 사용자만의 유니크한 QR 코드를 생성합니다.
--- src/client.d.ts
+++ src/client.d.ts
@@ -4,6 +4,13 @@ interface ImportMetaEnv {
    */
   readonly VITE_ALCHEMEY_API_KEY: string;
   readonly VITE_SERVER_BASE_URL: string;
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
   // 다른 환경 변수들에 대한 타입 정의...
 }
 
