b4d9565b061b80db40fdf22bd8970f294fbe7920
bluejoyq <bluejoy32@gmail.com>
2023-11-18 05:33:45 +0900

    feat: 데이터 옵션 추가

--- src/presentation/make/steps/DataStep.tsx
+++ src/presentation/make/steps/DataStep.tsx
@@ -1,6 +1,6 @@
 import { css } from "@emotion/react";
-import { Box, Input } from "@mui/joy";
-import { ReactElement } from "react";
+import { Box, Input, Select, Option } from "@mui/joy";
+import { ReactElement, useState } from "react";
 import { useNftQrFormContext } from "../hooks/useNftQrFormContext";
 import { NftPreview } from "@/presentation/common/components/NftPreview";
 import { AppButton } from "@/presentation/common/components/AppButton";
@@ -9,15 +9,53 @@ interface DataStepProps {
   onNext: () => void;
 }
 export const DataStep = ({ onNext }: DataStepProps): ReactElement => {
-  const { register, watch } = useNftQrFormContext();
+  const { register, watch, setValue } = useNftQrFormContext();
   const nft = watch("nft");
+  const address = watch("address");
+  const [readOnly, setReadOnly] = useState(false);
+
+  const handleSelectChange = (
+    _: any,
+    val: "address" | "addressUrl" | "nftUrl" | "free" | null
+  ) => {
+    if (val == "free" || val == null) {
+      setReadOnly(false);
+      return;
+    }
+    setReadOnly(true);
+    switch (val) {
+      case "address":
+        setValue("qrData", address);
+        break;
+      case "addressUrl":
+        setValue("qrData", `https://opensea.io/${address}`);
+        break;
+      case "nftUrl":
+        setValue(
+          "qrData",
+          `https://opensea.io/assets/${nft?.contract.address}/${nft?.tokenId}`
+        );
+        break;
+    }
+  };
   return (
-    <>
+    <Box css={styles.container}>
       <NftPreview nft={nft} />
-      <Box css={dataContainerStyles}>
+      <Select
+        placeholder="Recommended Data"
+        css={styles.select}
+        onChange={handleSelectChange}
+      >
+        <Option value="address">Address</Option>
+        <Option value="addressUrl">OpenSea Address URL</Option>
+        <Option value="nftUrl">OpenSea NFT URL</Option>
+        <Option value="free">Free</Option>
+      </Select>
+      <Box css={styles.dataContainer}>
         <Input
           maxLength={50}
           placeholder="Enter data to be encoded in QR code"
+          readOnly={readOnly}
           {...register("qrData", {
             required: true,
             maxLength: 50,
@@ -25,11 +63,24 @@ export const DataStep = ({ onNext }: DataStepProps): ReactElement => {
         />
         <AppButton onClick={onNext}>Next</AppButton>
       </Box>
-    </>
+    </Box>
   );
 };
-const dataContainerStyles = css`
-  display: grid;
-  grid-template-columns: 4fr 1fr;
-  column-gap: 10px;
-`;
+
+const styles = {
+  container: css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+  `,
+  select: css`
+    width: 300px;
+  `,
+  dataContainer: css`
+    display: grid;
+    grid-template-columns: 4fr 1fr;
+    column-gap: 10px;
+    width: 100%;
+  `,
+};
